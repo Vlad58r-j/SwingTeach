7cbaf8a47140a71b6ccd88960f40b6c9bbf3f6d5
vlad5 <vlad58r@gmail.com>
2020-12-23 19:12:52 +0300

    Ввывел в окне поля для ввода, подписал предназначение поля, создал большое поле ввода. Мне нужно научиться Выравнивать по столбцам и строкам элементы. Видео:https://www.youtube.com/watch?v=JaYLPGItruo&list=PL786bPIlqEjTN-Sy8uMLQ6s85qNxP1hAp&index=10

---
+++ src/ProjectInSchool/Projects.java
@@ -0,0 +1,10 @@
+package ProjectInSchool;
+
+import javax.swing.*;
+
+public class Projects {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        JPanel jPanel = new JPanel();
+    }
+}
--- src/Swing/Main1.java
+++ src/Swing/Main1.java
@@ -5,13 +5,21 @@ import java.awt.*;
 
 public class Main1 {
     static JFrame jFrame = getFrame();
+
     static JPanel jPanel = new JPanel();
 
     public static void main(String[] args) {
         jFrame.add(jPanel);
+        jPanel.add(new JLabel("Фамилия"));
+        jPanel.add(new JTextField(20));
+        jPanel.add(new JPasswordField(20));
         JTextField jTextField1 = new JTextField();
+        JTextArea jTextArea = new JTextArea(2,15);
+        jTextArea.setLineWrap(true);
         jTextField1.getText();
+        jTextField1.setBackground(Color.black);
         jPanel.revalidate();
+
     }
 
     static JFrame getFrame() {
